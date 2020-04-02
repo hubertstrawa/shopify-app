80e9caf52f74b580e7c708ec7c37c90e26d82e30
Vinyljam <hubertstrawa@gmail.com>
2020-04-02 10:55:41 +0200

    adding password

--- export.php
+++ export.php
@@ -1,39 +1,37 @@
 <?php
     require("inc/functions.php");
 
-    $requests = $_GET;
-    $hmac = $_GET['hmac'];
-    $serializeArray = serialize($requests);
-    $requests = array_diff_key($requests, array('hmac' => ''));
-
-    $token = "e7295b7279770956ed985ef1c94db645";
-    $shop = "hubertstrawa-testapp";
-
-    $collectionList = shopify_call($token, $shop, "/admin/api/2020-04/custom_collections.json", array(), 'GET');
-    $collectionList = json_decode($collectionList['response'], JSON_PRETTY_PRINT);
-    $collection_id = $collectionList['custom_collections'][0]['id'];
-
-    echo '<p>Collection ID: ' . $collection_id . 
-
-    $collects = shopify_call($token, $shop, "/admin/api/2020-04/collects.json", array("collection_id"=>$collection_id), 'GET');
-    $collects = json_decode($collects['response'], JSON_PRETTY_PRINT);
-
-    foreach ($collects as $collect) {
-        foreach ($collect as $key => $value) {
-            $products = shopify_call($token, $shop, "/admin/api/2020-04/products/" . $value['product_id'] . ".json", array("collection_id"=>$collection_id), 'GET');
-            $products = json_decode($products['response'], JSON_PRETTY_PRINT);
-
-            echo $products['product']['title'] . '<br />';
-        }
-    }
-
     $thisUrl = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
     $pass = $_GET['password'];
 
     echo $passIndex;
 
     if (strpos($thisUrl, $pass) !== false) {
-        echo '<br/>It has password<br/>';
+        $requests = $_GET;
+        $hmac = $_GET['hmac'];
+        $serializeArray = serialize($requests);
+        $requests = array_diff_key($requests, array('hmac' => ''));
+
+        $token = "e7295b7279770956ed985ef1c94db645";
+        $shop = "hubertstrawa-testapp";
+
+        $collectionList = shopify_call($token, $shop, "/admin/api/2020-04/custom_collections.json", array(), 'GET');
+        $collectionList = json_decode($collectionList['response'], JSON_PRETTY_PRINT);
+        $collection_id = $collectionList['custom_collections'][0]['id'];
+
+        echo '<p>Collection ID: ' . $collection_id . 
+
+        $collects = shopify_call($token, $shop, "/admin/api/2020-04/collects.json", array("collection_id"=>$collection_id), 'GET');
+        $collects = json_decode($collects['response'], JSON_PRETTY_PRINT);
+
+        foreach ($collects as $collect) {
+            foreach ($collect as $key => $value) {
+                $products = shopify_call($token, $shop, "/admin/api/2020-04/products/" . $value['product_id'] . ".json", array("collection_id"=>$collection_id), 'GET');
+                $products = json_decode($products['response'], JSON_PRETTY_PRINT);
+
+                echo $products['product']['title'] . '<br />';
+            }
+        }
     }
 
     echo $thisUrl;
